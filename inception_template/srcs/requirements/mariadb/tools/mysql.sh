de6917974e5aefb6db9025ceb919663c7597e7d6
Hithem EL KAMEL <hel-kame@made-f0Cr11s2.clusters.42paris.fr>
2023-10-09 16:46:42 +0200

    remove(inception_template)

--- inception_template/srcs/requirements/mariadb/tools/mysql.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    chown -R mysql:mysql /var/lib/mysql
-    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm
-    tfile=`mktemp`
-    [ ! -f "$tfile" ] && { return 1; }
-fi
